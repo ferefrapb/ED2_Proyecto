9471c6bd6d2d7a0050d78f0d38ec6dd6701fbdc4
ferefrapb <fpinguinator@hotmail.com>
2018-11-20 01:58:33 -0600

    Activity Mensajes y Contactos
    Se agrego bottom navigation

---
+++ app/src/main/java/com/example/carlos/ed2_proyecto/chatactivity.java
@@ -0,0 +1,42 @@
+package com.example.carlos.ed2_proyecto;
+
+import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class chatactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemReselectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener navListener =
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId())
+                    {
+                        case R.id.nav_mensajes:
+                            selectedFragment = new mensajesfragment();
+                            break;
+                        case R.id.nav_contactos:
+                            selectedFragment = new contactosfragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                }
+            };
+}
